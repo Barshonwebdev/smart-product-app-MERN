2f3786efc16ba58db6e8f94470058f6bbe86aa1f
Barshon <barshonweb@gmail.com>
2024-06-02 10:03:23 +0600

    user info shown on client from database

--- src/components/Dashboard/UserInfo.jsx
+++ src/components/Dashboard/UserInfo.jsx
@@ -1,8 +1,15 @@
+import { useEffect, useState } from 'react';
 import useAuth from '../../hooks/useAuth';
 
 const UserInfo = () => {
     const { user } = useAuth();
-  const { displayName, email, photoURL, emailVerified } = user;
+    const [userInformation,setUserInformation]=useState({});
+    useEffect(()=>{
+      fetch(`http://localhost:3100/user/${user?.email}`)
+      .then((res)=>res.json())
+      .then(data=>setUserInformation(data))
+    },[user?.email])
+  const {  photoURL, emailVerified } = user;
     return (
         <div className="card   max-h-screen  bg-base-100 shadow-xl my-3 md:my-0 me-3 ">
           <h1 className="text-2xl text-center text-gray-600 md:mt-3">
@@ -12,9 +19,9 @@ const UserInfo = () => {
             <img src={photoURL} className="rounded-xl" />
           </figure>
           <div className=" items-center text-gray-600 mx-2 text-center">
-            <h2 className="my-3 ">Username: {displayName}</h2>
+            <h2 className="my-3 ">Username: {userInformation?.name}</h2>
             <p className="mb-3 md:mb-2 ">
-              Email: <span className="font-semibold">{email}</span>
+              Email: <span className="font-semibold">{userInformation?.email}</span>
             </p>
             {emailVerified ? <p>Verified: Yes</p> : <p>Verified: No</p>}
             <div><button className='btn btn-primary my-3 btn-sm'>Edit Profile</button></div>
