8f9e8ab7ae9d98abad1cb044fbde29046b09e7bc
Barshon <barshonweb@gmail.com>
2024-06-02 15:36:47 +0600

    edit profile page functionality implemented

--- src/Pages/Dashboard/EditProfile.jsx
+++ src/Pages/Dashboard/EditProfile.jsx
@@ -1,27 +1,50 @@
 import { useLoaderData } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const EditProfile = () => {
-    const userData=useLoaderData();
-    const handleEditProfile=(e)=>{
-        e.preventDefault();
-        const form=e.target;
-        const name=form.name.value;
-        const age=form.age.value;
-        const phonenumber=form.phonenumber.value;
-        const photo=form.photo_url.value;
+  const userData = useLoaderData();
+  const handleEditProfile = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const age = form.age.value;
+    const phonenumber = form.phonenumber.value;
+    const photo = form.photo_url.value;
 
-        const userInformation={
-            name,age,email:userData?.email,phonenumber,photo
+    const userInformation = {
+      name,
+      age,
+      email: userData?.email,
+      phonenumber,
+      photo,
+    };
+    console.log(userInformation);
+    fetch(`http://localhost:3100/user/${userData?.email}`, {
+      method: "PATCH",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(userInformation),
+    }).then(res=>res.json())
+    .then(data=>{
+        if(data.modifiedCount){
+        Swal.fire('Profile updated') }
+        else {
+            Swal.fire('Please make a change to edit')
         }
-        console.log(userInformation);
-    }
-    return (
-        <div>
+    })
+     
+  };
+  return (
+    <div>
       <h1 className="my-3 text-2xl text-gray-500 text-center"> Edit Profile</h1>
       <div className="hero  ">
         <div className="hero-content  flex-col lg:flex-row-reverse">
           <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-            <form onSubmit={handleEditProfile} className="card-body grid grid-cols-2 gap-x-10">
+            <form
+              onSubmit={handleEditProfile}
+              className="card-body grid grid-cols-2 gap-x-10"
+            >
               <div className="form-control col-span-2">
                 <label className="label">
                   <span className="label-text">User Name</span>
@@ -75,10 +98,7 @@ const EditProfile = () => {
                   required
                 />
               </div>
-             
-             
-              
-             
+
               <div className="form-control col-span-2">
                 <label className="label">
                   <span className="label-text">Photo URL</span>
@@ -92,17 +112,18 @@ const EditProfile = () => {
                   required
                 />
               </div>
-              
-              
+
               <div className="form-control mt-6 col-span-2">
-                <button className="btn text-white bg-blue-700">Save Profile</button>
+                <button className="btn text-white bg-blue-700">
+                  Save Profile
+                </button>
               </div>
             </form>
           </div>
         </div>
       </div>
     </div>
-    );
+  );
 };
 
 export default EditProfile;
